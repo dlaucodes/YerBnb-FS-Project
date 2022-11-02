7631e73c70e82e41b9f115669ec2c3d3643efa14
Chun Lau <dlaucodes@gmail.com>
2022-11-02 16:11:42 -0400

    fix listings

--- frontend/src/components/ListingShow/index.js
+++ frontend/src/components/ListingShow/index.js
@@ -30,27 +30,33 @@ const ListingShow = () => {
         <h1>{item.title}</h1>
         </div>
         <div className="photo-container">
-            <div className="photo-container1"
-            >
+            {item.photoUrls.length > 0 && 
+            <div className="photo-container1">
                 <img src={`${item.photoUrls[0].imgUrl}`} className="photo-main"/>
+            </div>} 
+
+            {item.photoUrls.length > 0 &&
+
+            <div className="photo-container2" >
+            {item.photoUrls.map((photo, i) => {
+                    if ( i === 5 ) {
+                        return
+                    } else if (i % 2 !== 0) {
+                        return (
+                            <div className="top-photo">
+                                <img src={`${photo.imgUrl}`} className="photo-small"/>
+                            </div>
+                        )
+                    } else if (i % 2 === 0 && i !== 0) {
+                        return (
+                            <div className="bottom-photo">
+                                <img src={`${photo.imgUrl}`} className="photo-small"/>
+                            </div>
+                        )
+                    }
+            })}
             </div>
-            <div className="photo-container2">
-                <div className="top-photo"
-                >
-                    <img src={`${item.photoUrls[1].imgUrl}`} className="photo-small"/>
-                </div>
-                <div className="bottom-photo">
-                <img src={`${item.photoUrls[2].imgUrl}`} className="photo-small"/>
-                </div>
-            </div>
-            <div className="photo-container3">
-                <div className="top-photo">
-                    <img src={`${item.photoUrls[3].imgUrl}`} className="photo-small"/>
-                </div>
-                <div className="bottom-photo">
-                    <img src={`${item.photoUrls[4].imgUrl}`} className="photo-small"/>
-                </div>
-            </div>
+            }
         </div>
 
         <div className="hr-divider">
