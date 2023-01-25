64c206790981ed16fd28f22124d07f60fe890928
Chun Lau <dlaucodes@gmail.com>
2023-01-24 22:36:14 -0500

    spacing close

--- frontend/src/components/ListingShow/index.js
+++ frontend/src/components/ListingShow/index.js
@@ -33,20 +33,39 @@ const ListingShow = () => {
             <div className="photo-container1">
                 <img src={`${item.photoUrls[0].imgUrl}`} className="photo-main"/>
             </div>} 
-
+            <div className="right-photocontainer">
             {item.photoUrls.length > 0 &&
-
+           
             <div className="photo-container2" >
             {item.photoUrls.map((photo, i) => {
-                        
-                return (         
+                if ( i === 1 || i === 3){      
+                return (
+                            
                 <img src={`${photo.imgUrl}`} className="photo-small"/>
+                
                 )
-                         
+                }     
                      
             })}
             </div>
             }
+
+            {item.photoUrls.length > 0 &&
+
+            <div className="photo-container3" >
+            {item.photoUrls.map((photo, i) => {
+                if ( i === 2 || i === 4){      
+                return (
+                            
+                <img src={`${photo.imgUrl}`} className="photo-small"/>
+               
+                )
+                }     
+                     
+            })}
+            </div>
+            }
+            </div>
         </div>
 
         <div className="hr-divider">
@@ -86,6 +105,26 @@ export default ListingShow;
     //  <div className="photo-container2" >
     //         {item.photoUrls.map((photo, i) => {
     //                 if ( i === 5 ) {
+    //                     return
+    //                 } else if (i % 2 !== 0) {
+    //                     return (
+    //                         <div className="top-photo">
+    //                             <img src={`${photo.imgUrl}`} className="photo-small"/>
+    //                         </div>
+    //                     )
+    //                 } else if (i % 2 === 0 && i !== 0) {
+    //                     return (
+    //                         <div className="bottom-photo">
+    //                             <img src={`${photo.imgUrl}`} className="photo-small"/>
+    //                         </div>
+    //                     )
+    //                 }
+    //         })}
+    //         </div>
+
+    //  <div className="photo-container2" >
+    //         {item.photoUrls.map((photo, i) => {
+    //                 if ( i === 1 || i === 2 ) {
     //                     return
     //                 } else if (i % 2 !== 0) {
     //                     return (
