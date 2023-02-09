bca5496d48c559070155cb9aa4a682100cd45e25
Chun Lau <dlaucodes@gmail.com>
2023-02-08 21:26:10 -0500

    creating map

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,10 +1,42 @@
+import React, {useEffect, useRef, useState} from 'react';
+import { Wrapper } from '@googlemaps/react-wrapper';
+import './Map.css';
 
-
-function Map(){
+function Map({
     
+    
+}) {
+    const [map, setMap] = useState(null);
+    const mapRef = useRef(null)
+
+
+    useEffect(()=>{
+       if(!map){
+           setMap(new window.__googleMapsCallback.maps.Map(mapRef.current, {
+               center: {
+                   lat: 40.74363402543966,
+                   lng: -73.98377122848856
+               },
+               zoom: 13,
+               mapId: "49aa6f67e21bd8eb",
+               gestureHandling: "greedy",
+               clickableIcons: false,
+               diableDefaultUI: true,
+               ...mapOptions,
+           }))
+       } 
+    })
+
+
+    useEffect(()=>{
+        
+    }
 }
 
-function MapWrapper(){
+
+
+
+function MapWrapper(props){
 
 }
 
