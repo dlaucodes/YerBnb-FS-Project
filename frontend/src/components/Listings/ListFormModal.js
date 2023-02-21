145ee244956926ba4c89dae896d1f5764a12fb1a
Chun Lau <dlaucodes@gmail.com>
2023-02-21 17:19:05 -0500

    create listing works from homepage

--- frontend/src/components/Listings/ListFormModal.js
+++ frontend/src/components/Listings/ListFormModal.js
@@ -7,8 +7,17 @@ import * as sessionActions from "../../store/session";
 const ListForm = ({setShowListFormModal}) =>{
     const dispatch = useDispatch();
     const [title,setTitle] = useState ("")
-    const [photoFiles, setPhotoFiles] = useState([]);
-    const [photoUrl, setPhotoUrl] = useState(null)
+    const [photoFile, setPhotoFile] = useState(null);
+    const [photoFile2, setPhotoFile2] = useState (null);
+    const [photoFile3, setPhotoFile3] = useState (null);
+    const [photoFile4, setPhotoFile4] = useState (null);
+    const [photoFile5, setPhotoFile5] = useState (null);
+    const [photoUrl, setPhotoUrl] = useState(null);
+    const [photoUrl2, setPhotoUrl2] = useState(null);
+    const [photoUrl3, setPhotoUrl3] = useState(null);
+    const [photoUrl4, setPhotoUrl4] = useState(null);
+    const [photoUrl5, setPhotoUrl5] = useState(null);
+    const [files, setFiles] = useState(null);
     const [price, setPrice] = useState("")
     const [description, setDescription] = useState("")
     const [location, setLocation] = useState("")
@@ -30,8 +39,11 @@ const ListForm = ({setShowListFormModal}) =>{
         formData.append('listing[owner_id]', owner.id);
 
 
-        if (photoFiles) {
-            formData.append('listing[photos]', photoFiles)
+        if (files && files.length > 0 && files.length < 6) {
+            files.forEach((file)=> {
+                formData.append('listing[photos][]', file)
+
+            })
         }
         
         const response = await fetch('api/listings', {
@@ -42,7 +54,7 @@ const ListForm = ({setShowListFormModal}) =>{
         if (response.ok) {
             const message = await response.json();
             setTitle("")
-            setPhotoFiles(null)
+            setPhotoFile(null)
             setPhotoUrl(null)
             setDescription("")
             setPrice("")
@@ -58,17 +70,64 @@ const ListForm = ({setShowListFormModal}) =>{
     
     
     const handleFile = e =>{
-        let SubmitPhotoFiles = [];
-        e.target.files.forEach((file) => {
-            SubmitPhotoFiles.push(file);
-        })
+    //     let SubmitPhotoFiles = [];
+    //     e.target.files.forEach((file) => {
+    //         SubmitPhotoFiles.push(file);
+    //     })
         const file = e.currentTarget.files[0];
         const file2 = e.currentTarget.files[1];
         const file3 = e.currentTarget.files[2];
         const file4 = e.currentTarget.files[3];
         const file5 = e.currentTarget.files[4];
-        setPhotoFiles(SubmitPhotoFiles);
+        const currentFiles = e.currentTarget.files;
+        const filesList = [];
+        for (let i = 0; i < currentFiles.length; i++) {
+        filesList.push(currentFiles[i]);
     }
+        setFiles(filesList);
+        if (file) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        setPhotoFile(file);
+        setPhotoUrl(fileReader.result);
+      };
+    } else setPhotoUrl(null);
+    if (file2) {
+      const fileReader2 = new FileReader();
+      fileReader2.readAsDataURL(file2);
+      fileReader2.onload = () => {
+        setPhotoFile2(file2);
+        setPhotoUrl2(fileReader2.result);
+      };
+    } else setPhotoUrl2(null);
+    if (file3) {
+      const fileReader3 = new FileReader();
+      fileReader3.readAsDataURL(file3);
+      fileReader3.onload = () => {
+        setPhotoFile3(file3);
+        setPhotoUrl3(fileReader3.result);
+      };
+    } else setPhotoUrl3(null);
+    if (file4) {
+      const fileReader4 = new FileReader();
+      fileReader4.readAsDataURL(file4);
+      fileReader4.onload = () => {
+        setPhotoFile4(file4);
+        setPhotoUrl4(fileReader4.result);
+      };
+    } else setPhotoUrl4(null);
+    if (file5) {
+      const fileReader5 = new FileReader();
+      fileReader5.readAsDataURL(file5);
+      fileReader5.onload = () => {
+        setPhotoFile5(file5);
+        setPhotoUrl5(fileReader5.result);
+      };
+    } else setPhotoUrl5(null);
+  }
+
+    
     
     return (
         <>
