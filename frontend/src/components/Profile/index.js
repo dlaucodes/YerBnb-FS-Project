7c0c74473f9e2c49eea51fa9f28620ef12a72581
Chun Lau <dlaucodes@gmail.com>
2023-02-20 22:55:01 -0500

    set id

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -3,11 +3,11 @@ import { useParams } from 'react-router-dom';
 import './Profile.css';
 
 const ProfileDetails = () => {
-    const { id } = useParams
+    const { id } = useParams();
     return (
         <>
         <div className="testing">
-        Profile details
+        Profile details - { id }
         </div> 
         </>
      );
