a075a1b99f041617d211f573943e83914489ff8a
Chun Lau <dlaucodes@gmail.com>
2023-03-19 16:40:17 -0400

    temporary googlemap

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -4,11 +4,11 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api';
 const MapContainer = () => {
   
   const mapStyles = {        
-    height: "90vh",
+    height: "auto",
     width: "100%"};
   
   const defaultCenter = {
-    lat: 41.3851, lng: 2.1734
+    lat: 40.730610, lng: -73.935242
   }
   
   return (
