9fbe41ea5e97309a3a47dc6707d5d5fdb66e6e73
Chun Lau <dlaucodes@gmail.com>
2023-03-27 18:42:10 -0400

    reseed db with lng lng test

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { NavItem } from 'react-bootstrap';
+
 
 const MapContainer = ({listingItem}) => {
   
@@ -11,17 +13,32 @@ const MapContainer = ({listingItem}) => {
   const defaultCenter = {
     lat: 40.730610, lng: -73.935242
   }
+
+//   const locations = [{location: {lat: listingItem.lat, lng: listingItem.lng}}]
+
+//   console.log(listingItem)
+
+//   console.log(locations)
   
   return (
      <LoadScript
        googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}>
+
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={12}
           center={defaultCenter}
           disableDefaultUI={true}
-          clickableIcons={false}  
-        />
+          clickableIcons={false}>
+              {/* {
+              locations.map(item =>{
+              return (
+              
+                  <Marker position={item.location}/>
+                  )
+              })
+            } */}
+        </GoogleMap>  
      </LoadScript>
   )
 }
