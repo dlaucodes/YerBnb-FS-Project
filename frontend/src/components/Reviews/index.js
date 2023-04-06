dd6458b4558caf7cb38549530281fc37ac333297
Chun Lau <dlaucodes@gmail.com>
2023-04-05 21:00:06 -0400

    review component

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,3 +1,8 @@
 import "./Review.css"
 import React from "react"
 import { useParams, Link } from "react-router-dom"
+
+const Reviews= (props)=>{
+    const currentUser = useSelector((state)=> state.session.user)
+    const listings = useSelector(state=> state.listing)
+}
