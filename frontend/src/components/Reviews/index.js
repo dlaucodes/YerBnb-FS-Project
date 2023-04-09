8b3eb6a6b592721e240c76a2d0b1d9decbe572c7
Chun Lau <dlaucodes@gmail.com>
2023-04-09 16:44:00 -0400

    add columns to reservations

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -2,7 +2,7 @@ import "./Review.css"
 import React from "react"
 import { useParams, Link } from "react-router-dom"
 
-const Reviews= (props)=>{
+const Reviews = (props)=>{
     const currentUser = useSelector((state)=> state.session.user)
     const listings = useSelector(state=> state.listing)
 }
