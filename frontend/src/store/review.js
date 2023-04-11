ae76a03d479c43da7e394ad0a884ace3435bebf3
Chun Lau <dlaucodes@gmail.com>
2023-04-11 16:25:31 -0400

    review store

---
+++ frontend/src/store/review.js
@@ -0,0 +1,42 @@
+import csrfFetch from "./csrf";
+
+const RECEIVE_REVIEW = "reviews/RECEIVE_REVIEW";
+const RECEIVE_REVIEWS = "reviews/RECEIVE_REVIEWS";
+
+
+export const receiveReview = (review) =>{
+    return {
+        type: RECEIVE_REVIEW,
+        payload: review
+    }
+};
+
+export const receiveReviews = (reviews) =>{
+    return {
+        type: RECEIVE_REVIEWS,
+        payload: reviews
+    }
+}
+
+export const createReview = (formData) => async (dispatch)=>{
+    const res = await csrfFetch("/api/reviews", {
+        method: "POST",
+        body: formData
+    });
+    const data = await res.json();
+    dispatch(receiveReview(data));
+    return res;
+}
+
+const reviewReducer = (state = [], action) =>{
+    switch(action.type){
+        case RECEIVE_REVIEW:
+            return {...state, review: null}
+        case RECEIVE_REVIEWS:
+            return {...state, ...action.payload}
+        default:
+            return state;
+    }
+}
+
+export default reviewReducer;
