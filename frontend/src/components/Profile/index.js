badfe836a508adf002a1be7c5a2482fbae5021a3
Chun Lau <dlaucodes@gmail.com>
2023-04-12 02:33:31 -0400

    create review cards

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -136,11 +136,17 @@ const ProfileDetails = () => {
                     </div>
                 </div>
                 <div className="reviews">
+                            <div className="review-header">
+                                <div className="review-star">
+                                    <svg viewBox="0 0 32 32" height="16px" width="16px"><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" ></path></svg>
 
-                    <svg viewBox="0 0 32 32" height="16px" width="16px"><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" ></path></svg>
-                    <div className="lives-in-text">Reviews
+                                </div>
+                            <div className="review-title">
+                                Reviews
                             </div>
-                        <div>
+
+                            </div>
+                    <div>
     
                         {reviewsArray.map((review)=>{
                             console.log(review)
@@ -154,7 +160,7 @@ const ProfileDetails = () => {
                                 )
                             }
                         })}
-                        </div>
+                    </div>
                 </div>
                 <div className="right-divider"></div>
                 <div className="user-trips">
