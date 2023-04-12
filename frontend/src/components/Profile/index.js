a22c9619162fa5c94be3a137434a4ffc567c77ee
Chun Lau <dlaucodes@gmail.com>
2023-04-12 02:20:21 -0400

    reviews div

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -136,8 +136,12 @@ const ProfileDetails = () => {
                     </div>
                 </div>
                 <div className="reviews">
+
                     <svg viewBox="0 0 32 32" height="16px" width="16px"><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" ></path></svg>
                     <div className="lives-in-text">Reviews
+                            </div>
+                        <div>
+    
                         {reviewsArray.map((review)=>{
                             console.log(review)
                             if ((review.userId === currentUser.id)){
@@ -150,7 +154,7 @@ const ProfileDetails = () => {
                                 )
                             }
                         })}
-                    </div>
+                        </div>
                 </div>
                 <div className="right-divider"></div>
                 <div className="user-trips">
@@ -218,5 +222,7 @@ const ProfileDetails = () => {
 }
  
 export default ProfileDetails
+
+
 
  
