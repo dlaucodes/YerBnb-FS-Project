89b20c55c9d804d8943afa8bd522d5f0b7363a56
Chun Lau <dlaucodes@gmail.com>
2023-04-18 22:10:58 -0400

    review-card profile

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -164,14 +164,17 @@ const ProfileDetails = () => {
                             if ((review.userId === currentUser.id)){
                                 return(
                                         <div className="review-card">
-                                            <div className="review-listing-pic">
-                                                <img src={`${review.listingPic}` } alt=""/>
+                                            <div className="review-card-left">
+                                                <div className="review-listing-pic">
+                                                    <img src={`${review.listingPic}` } alt=""/>
+                                                </div>
                                             </div>
+                                            <div className="review-card-right">
                                             <div className="profile-review">
                                                 {review.body}
                                             </div>
                                             <button id="profile-listing-delete" onClick={() => {handleReviewDelete(review.id)}}>delete</button>
-                                    
+                                            </div>
                                         </div>
                                 
                                 )
