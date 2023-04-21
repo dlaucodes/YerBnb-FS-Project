856d372c66ae64ce51820bc94e2d2a1d38b087a4
Chun Lau <dlaucodes@gmail.com>
2023-04-21 16:00:09 -0400

    create star

--- frontend/src/components/Reviews/Stars.js
+++ frontend/src/components/Reviews/Stars.js
@@ -1,1 +1,27 @@
 import { useState } from "react";
+
+const StarRating = ()=>{
+    const [rating, setRating] = useState(5);
+    const [hover, setHover] = useState(0);
+
+    return(
+        <div className= "rating">
+            {[...Array(5)].map((star, i)=>{
+                i+=1
+                return(
+                    <button id="star" type="button" key={i}
+                        className={i <= (hover || rating) ? "true" : "false"}
+                        onClick={()=> setRating(i)}
+                        onMouseEnter={()=> setHover(i)}
+                        onMouseLeave={()=> setHover(rating)}
+                        >
+                           <span className="star">&#9733;</span> 
+                        </button>
+                )
+            })
+            }
+        </div>
+    )
+}
+
+export default StarRating;
