36b97fe129ef8768f05bb893517976f1ee2f5d14
Chun Lau <dlaucodes@gmail.com>
2023-05-10 14:28:35 -0400

    profilebutton user pic:

--- frontend/src/components/NavBar/ProfileButton.js
+++ frontend/src/components/NavBar/ProfileButton.js
@@ -59,17 +59,17 @@ return (
         <g fill="none" fillRule="nonzero"><path d="m2 16h28"></path><path d="m2 24h28"></path><path d="m2 8h28"></path></g>
         </svg>
 
-        {currentUser.photoUrl ? (
+        {!currentUser || !currentUser.photoUrl ? (
           <>
-          <div className='profile-photo' viewBox="0 0 32 32">
-            {<img src={currentUser.photoUrl}/>}
-          </div>
+        <svg className='profile-icon' width={30} height={30} viewBox="0 0 32 32" fill={'#717171'}>
+          <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+        </svg>
         </>
           ) : (
             <>
-        <svg className='profile-icon' width={30} height={30} viewBox="0 0 32 32" fill={'#e0024c'}>
-          <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
-        </svg>
+          <div className='profile-photo' viewBox="0 0 32 32">
+            {<img src={currentUser.photoUrl}/>}
+          </div>
             </>
           )}
       </button>
