8dea30fa30df1223781e549f7eee4e5b422d10ff
Chun Lau <dlaucodes@gmail.com>
2023-05-10 18:55:42 -0400

    test mapfix

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -53,7 +53,7 @@ const MapContainer = ({listingArray, reviewsArray}) => {
   return (
       <>
      <LoadScript
-       googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}>
+       googleMapsApiKey={AIzaSyDw0iAzn2l3U2yAk70laf04TfH9Nc1KhDI}>
 
         <GoogleMap
           mapContainerStyle={mapStyles}
