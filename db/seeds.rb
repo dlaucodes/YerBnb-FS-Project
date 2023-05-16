7f715e6e99e07d97f6f115374cd48495152629c8
Chun Lau <dlaucodes@gmail.com>
2023-05-16 19:00:33 -0400

    seed listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -254,53 +254,59 @@ demo_listing_11.photos.attach(io: demo_photo3, filename: 'gs3.webp')
 demo_photo4 = File.open("app/assets/images/Gstudio/gs4.webp")
 demo_listing_11.photos.attach(io: demo_photo4, filename: 'gs4.webp')
 
-#12
-demo_listing1 = Listing.create!(price: "320", title: "Cabin in the City (GQ featured Best NYC Yerbnbs)", description: 
+#12- Sonder-4
+demo_listing_12 = Listing.create!(price: "700", title: "Sonder Battery Park | Studio Apartment", description: 
 
-"Spacious and cozy historical flat apartment. Has its own private entrance with 1000 sqft of space. Exposed wood beams with designer mid-century furniture throughout the apartment and a newly renovated kitchen. Enjoy easy walking distance to the LES, East Village and Tribeca and quiet block with great nightlife around the corner. Recently featured on GQ as one of the best rentals in the city!", owner_id: "6", lng: -73.994379, lat: 40.709808, guests: 5, bedrooms: 2, beds: 3, baths: 2, city: "New York", wifi: true, kitchen: true, pets_allowed: true, state: "New York")
+"From the moment you see the waterfront from the roof, you’ll know you made the right choice. Your space includes a kitchenette and dining nook. There’s also a (seasonal) rooftop pool, fitness center, golf simulator, and co-working space. You’ll be right across from The Battery — a waterfront oasis with 195,000 square feet of perennial gardens. The New York Stock Exchange, Wall Street, and One World Trade Center are also a quick walk away.
+", owner_id: "4", lng: -74.01720, lat: 40.70709, guests: 2, bedrooms: 1, beds: 1, baths: 1, city: "New York", wifi: true, kitchen: true, pets_allowed: true, state: "New York")
 
-demo_photomain = File.open("app/assets/images/Gstudio/gs0.webp")
-demo_listing1.photos.attach(io: demo_photomain, filename: 'citycabin0.webp')
-demo_photo1 = File.open("app/assets/images/CityCabin/citycabin1.webp")
-demo_listing1.photos.attach(io: demo_photo1, filename: 'citycabin1.webp')
-demo_photo2 = File.open("app/assets/images/CityCabin/citycabin2.webp")
-demo_listing1.photos.attach(io: demo_photo2, filename: 'citycabin2.webp')
-demo_photo3 = File.open("app/assets/images/CityCabin/citycabin3.webp")
-demo_listing1.photos.attach(io: demo_photo3, filename: 'citycabin3.webp')
-demo_photo4 = File.open("app/assets/images/CityCabin/citycabin4.webp")
-demo_listing1.photos.attach(io: demo_photo4, filename: 'citycabin4.webp')
+demo_photomain = File.open("app/assets/images/Sonder/s0.webp")
+demo_listing_12.photos.attach(io: demo_photomain, filename: 's0.webp')
+demo_photo1 = File.open("app/assets/images/Sonder/s1.webp")
+demo_listing_12.photos.attach(io: demo_photo1, filename: 's1.webp')
+demo_photo2 = File.open("app/assets/images/Sonder/s2.webp")
+demo_listing_12.photos.attach(io: demo_photo2, filename: 's2.webp')
+demo_photo3 = File.open("app/assets/images/Sonder/s3.webp")
+demo_listing_12.photos.attach(io: demo_photo3, filename: 's3.webp')
+demo_photo4 = File.open("app/assets/images/Sonder/s4.webp")
+demo_listing_12.photos.attach(io: demo_photo4, filename: 's4.webp')
 
-#13
-demo_listing1 = Listing.create!(price: "320", title: "Cabin in the City (GQ featured Best NYC Yerbnbs)", description: 
+#13-5-Legal Airbnb Lovely Rare Garden Home w/ Loft#10303
+demo_listing_13 = Listing.create!(price: "444", title: "Legal Airbnb Lovely Rare Garden Home w/ Loft#10303", description: 
 
-"Spacious and cozy historical flat apartment. Has its own private entrance with 1000 sqft of space. Exposed wood beams with designer mid-century furniture throughout the apartment and a newly renovated kitchen. Enjoy easy walking distance to the LES, East Village and Tribeca and quiet block with great nightlife around the corner. Recently featured on GQ as one of the best rentals in the city!", owner_id: "6", lng: -73.994379, lat: 40.709808, guests: 5, bedrooms: 2, beds: 3, baths: 2, city: "New York", wifi: true, kitchen: true, pets_allowed: true, state: "New York")
+"Welcome to your unique and amazing Harlem home! An incredibly designed duplex apartment in trendy South Harlem with great eco-friendly functionality and bespoke architectural elements throughout including a large private garden & chef's kitchen featuring Wolf range & subzero fridge! Perfect family layout with kid's bunk room, crib and loft queen bed above the living area, plus an electric piano, Sonos sound system, giant TV, Miele washer/dryer for easy cleanup.
 
-demo_photomain = File.open("app/assets/images/CityCabin/citycabin0.webp")
-demo_listing1.photos.attach(io: demo_photomain, filename: 'citycabin0.webp')
-demo_photo1 = File.open("app/assets/images/CityCabin/citycabin1.webp")
-demo_listing1.photos.attach(io: demo_photo1, filename: 'citycabin1.webp')
-demo_photo2 = File.open("app/assets/images/CityCabin/citycabin2.webp")
-demo_listing1.photos.attach(io: demo_photo2, filename: 'citycabin2.webp')
-demo_photo3 = File.open("app/assets/images/CityCabin/citycabin3.webp")
-demo_listing1.photos.attach(io: demo_photo3, filename: 'citycabin3.webp')
-demo_photo4 = File.open("app/assets/images/CityCabin/citycabin4.webp")
-demo_listing1.photos.attach(io: demo_photo4, filename: 'citycabin4.webp')
+P.S. This is a legal Airbnb.", owner_id: "5", lng: -73.99957, lat: 40.73071, guests: 7, bedrooms: 3, beds: 4, baths: 3, city: "New York", wifi: true, kitchen: true, pets_allowed: true, state: "New York")
 
-#14
-demo_listing1 = Listing.create!(price: "320", title: "Cabin in the City (GQ featured Best NYC Yerbnbs)", description: 
+demo_photomain = File.open("app/assets/images/BkLoft/l0.webp")
+demo_listing_13.photos.attach(io: demo_photomain, filename: 'l0.webp')
+demo_photo1 = File.open("app/assets/images/BkLoft/l1.webp")
+demo_listing_13.photos.attach(io: demo_photo1, filename: 'l1.webp')
+demo_photo2 = File.open("app/assets/images/BkLoft/l2.webp")
+demo_listing_13.photos.attach(io: demo_photo2, filename: 'l2.webp')
+demo_photo3 = File.open("app/assets/images/BkLoft/l3.webp")
+demo_listing_13.photos.attach(io: demo_photo3, filename: 'l3.webp')
+demo_photo4 = File.open("app/assets/images/BkLoft/l4.webp")
+demo_listing_13.photos.attach(io: demo_photo4, filename: 'l4.webp')
 
-"Spacious and cozy historical flat apartment. Has its own private entrance with 1000 sqft of space. Exposed wood beams with designer mid-century furniture throughout the apartment and a newly renovated kitchen. Enjoy easy walking distance to the LES, East Village and Tribeca and quiet block with great nightlife around the corner. Recently featured on GQ as one of the best rentals in the city!", owner_id: "6", lng: -73.994379, lat: 40.709808, guests: 5, bedrooms: 2, beds: 3, baths: 2, city: "New York", wifi: true, kitchen: true, pets_allowed: true, state: "New York")
+#14-Townhouse- 5
+demo_listing_14 = Listing.create!(price: "633", title: "Award-winning Townhouse in Williamsburg", description: 
 
-demo_photomain = File.open("app/assets/images/CityCabin/citycabin0.webp")
-demo_listing1.photos.attach(io: demo_photomain, filename: 'citycabin0.webp')
-demo_photo1 = File.open("app/assets/images/CityCabin/citycabin1.webp")
-demo_listing1.photos.attach(io: demo_photo1, filename: 'citycabin1.webp')
-demo_photo2 = File.open("app/assets/images/CityCabin/citycabin2.webp")
-demo_listing1.photos.attach(io: demo_photo2, filename: 'citycabin2.webp')
-demo_photo3 = File.open("app/assets/images/CityCabin/citycabin3.webp")
-demo_listing1.photos.attach(io: demo_photo3, filename: 'citycabin3.webp')
-demo_photo4 = File.open("app/assets/images/CityCabin/citycabin4.webp")
-demo_listing1.photos.attach(io: demo_photo4, filename: 'citycabin4.webp')
+"The Corner Townhouse is a spacious and modern oasis in the center of Williamsburg. Its beautiful award-winning design has been featured in Metropolis and Wallpaper* magazines. The refreshing design is coupled with luxury amenities and a private rooftop garden with views of the East River.
+
+Located in the heart of hip Williamsburg, the house is a sanctuary in the middle of what's happening.
+", owner_id: "5", lng: -73.96253, lat: 40.71783, guests: 6, bedrooms: 3, beds: 3, baths: 3.5, city: "Williamsburg", wifi: true, kitchen: true, pets_allowed: true, state: "New York")
+
+demo_photomain = File.open("app/assets/images/Townhouse/th0.webp")
+demo_listing_14.photos.attach(io: demo_photomain, filename: 'th0.webp')
+demo_photo1 = File.open("app/assets/images/Townhouse/th1.webp")
+demo_listing_14.photos.attach(io: demo_photo1, filename: 'th1.webp')
+demo_photo2 = File.open("app/assets/images/Townhouse/th2.webp")
+demo_listing_14.photos.attach(io: demo_photo2, filename: 'th2.webp')
+demo_photo3 = File.open("app/assets/images/Townhouse/th3.webp")
+demo_listing_14.photos.attach(io: demo_photo3, filename: 'th3.webp')
+demo_photo4 = File.open("app/assets/images/Townhouse/th4.webp")
+demo_listing_14.photos.attach(io: demo_photo4, filename: 'th4.webp')
 
 
 
