bba62be0c2ad2086a089d836135465fc233a235f
Chun Lau <dlaucodes@gmail.com>
2023-05-16 18:29:52 -0400

    footer text

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <footer className="navbar">
         <div className="footer-text">
-            This Airbnb clone is for educational purposes only.  Please do not include any sensitive information.
+            This Airbnb clone is for educational purposes only.  Please do not include any sensitive information. All listing information and pictures belong to respective owners.
         </div>
         </footer>
 
