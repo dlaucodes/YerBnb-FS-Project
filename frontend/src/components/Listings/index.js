6d193ffe327f55b10b92139b0152bf1070c9158c
Chun Lau <dlaucodes@gmail.com>
2023-05-17 02:47:52 -0400

    searchbar mobile

--- frontend/src/components/Listings/index.js
+++ frontend/src/components/Listings/index.js
@@ -8,13 +8,14 @@ import Profile from '../Profile'
 import MapContainer from "../Map";
 import { fetchReviews } from "../../store/review"
 import Search from "../Search";
+import '../../components/Search/Search.css'
 
 
 
-const ListingIndex = ({searchInput}) => {
+const ListingIndex = () => {
     const [list, setList] = useState(null);
     const dispatch = useDispatch();
-    const [search, setSearch] = useState(searchInput)
+    const [searchInput, setSearchInput] = useState("")
     
     // console.log(search)
     
@@ -57,6 +58,19 @@ const ListingIndex = ({searchInput}) => {
         
         return (
             <>
+            
+                <form className="search-bar">
+            <input className="search-input"
+            type="text"
+            action="search"
+            placeholder="search" onChange={(e)=> setSearchInput(e.target.value)}>
+                
+            </input>
+            <div className='search-icon'>
+                <svg viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" height="12px" width="12px" display="block" stroke="currentcolor" strokeWidth="5.33333" overflow="visible"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
+            </div>
+            </form>
+           
         <div className="listing-page">
             <div className="listing-filter">
                 {/* <div className="filter">
