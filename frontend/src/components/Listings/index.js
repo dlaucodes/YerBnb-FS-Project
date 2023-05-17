34634353b822f88ac5f2c502e4ee2d72fec409a6
Chun Lau <dlaucodes@gmail.com>
2023-05-17 02:16:11 -0400

    test moving search to main

--- frontend/src/components/Listings/index.js
+++ frontend/src/components/Listings/index.js
@@ -7,13 +7,16 @@ import "./listing.css"
 import Profile from '../Profile'
 import MapContainer from "../Map";
 import { fetchReviews } from "../../store/review"
+import Search from "../Search";
 
 
 
-const ListingIndex = () => {
+const ListingIndex = ({searchInput}) => {
     const [list, setList] = useState(null);
     const dispatch = useDispatch();
-   
+    const [search, setSearch] = useState(searchInput)
+    
+    // console.log(search)
     
     useEffect(() => {
         const res = csrfFetch('/api/listings').then(res => {
@@ -23,19 +26,37 @@ const ListingIndex = () => {
         });
     }, [list]);
     
-
+    
     useEffect(()=>{
         dispatch(fetchReviews())   
     }, [])
-
+    
     if (list){
-        let array = Object.values(list)
-        let listings = Object.values(array.reverse())
-        let listingsArray = Object.values(listings[0])
+        const array = Object.values(list)
+        const listings = Object.values(array.reverse())
+        const listingsArray = Object.values(listings[0])
         const showArray = listingsArray.reverse()
+        const filteredArray = showArray
+        
        
-    return (
-        <>
+        // .filter(item => (item.city).toLowerCase().includes(search))
+
+        
+    //     const showArray = showArray.filter((item) => {
+          
+    //     const lowercaseSearch = searchInput.toLowerCase();
+    //     const city = item.city ? item.city.toString() : "";
+      
+    //   return (
+    //     searchInput === "" || // Show all items if searchInput is empty
+    //     item.city.toLowerCase().includes(lowercaseSearch)
+        
+    //   );
+    // });
+    //     showArray is an array of objects that contain each listing's info
+        
+        return (
+            <>
         <div className="listing-page">
             <div className="listing-filter">
                 {/* <div className="filter">
@@ -44,14 +65,21 @@ const ListingIndex = () => {
             </div>
             <div className="listing-bottom">
                 <div className="listing-container">
-                    {showArray.map((item, i) => (
-                    <div key={i}>
+                
+                    {filteredArray
+                    // .filter((item)=>{
+                    //     return search === '' ? item 
+                    //     : item.city.includes(search);
+                    // })
+                    .map((item, i) => (
+                        <div key={i}>
+                        
                     <Listing listingItem={item} />
                     </div>
                     ))}
                 </div>
                 <div className="map">
-                    <MapContainer listingArray={showArray} />    
+                    <MapContainer listingArray={filteredArray} />    
                 </div>
             </div>
 
--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,27 +1,39 @@
 import './Search.css';
 import React, {useState} from 'react';
+import ListingIndex from '../Listings';
 
 
 const Search = ()=>{
     const [searchInput, setSearchInput] = useState("")
+    const listingIndexComponent = searchInput ? (
+    <ListingIndex searchInput={searchInput} />
+    ) : null;
 
-    const handleSearch = (e)=> {
-        setSearchInput();
-    }
 
-    return(
+
+    if(1 > 0){
+        return(
         <>
-        <form className="search-bar" action="search">
-            <div className="search-input">
-                Start your search
-            </div>
-            <div className='search-icon' onClick={()=>{handleSearch()}}>
+        <form className="search-bar">
+            <input className="search-input"
+            type="text"
+            action="search"
+            placeholder="search" onChange={(e)=> setSearchInput(e.target.value)}>
+                
+            </input>
+            <div className='search-icon'>
                 <svg viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" height="12px" width="12px" display="block" stroke="currentcolor" strokeWidth="5.33333" overflow="visible"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
             </div>
         </form>
+        {/* {listingIndexComponent} */}
         </>
-    )
 
+        )
+
+    }else{
+        <div className="hidden">
+        </div>
+    }
 
 }
 
